cf2d819dafa93f10d73f522a907992db713a9088
Carlos Junior <carlosjr.developer@outlook.com>
2020-09-12 17:19:09 -0300

    update commit(completion of the creation activity intro and splash screen.

---
+++ app/src/main/java/com/carlos/marvel/presentation/intros/IntroLoadingActivity.kt
@@ -0,0 +1,67 @@
+package com.carlos.marvel.presentation.intros
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import com.bumptech.glide.Glide
+import com.carlos.marvel.R
+import kotlinx.android.synthetic.main.activity_intro_loading.*
+import java.util.*
+
+@Suppress("DEPRECATION")
+class IntroLoadingActivity : AppCompatActivity() {
+
+    private var timer: Timer? = null
+
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_loading)
+        ocultarstatusBar()
+
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+
+        timer = Timer()
+
+        handler = Handler()
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                startActivity(
+                    Intent(
+                        this@IntroLoadingActivity,
+                        SlideActivity::class.java
+                    )
+                )
+                finish()
+            }
+        }, 6000)
+
+        fun onDestroy() {
+            super.onDestroy()
+            handler.removeCallbacksAndMessages(null)
+        }
+
+        Glide.with(this)
+            .load(R.drawable.marvel02)
+            .into(imageIntro)
+
+        Glide.with(this)
+            .load(R.drawable.giphy)
+            .into(imageViewLoad)
+    }
+
+    private fun ocultarstatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+}
---
+++ app/src/main/java/com/carlos/marvel/presentation/intros/SlideActivity.kt
@@ -0,0 +1,50 @@
+package com.carlos.marvel.presentation.intros
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.carlos.marvel.R
+import com.carlos.marvel.presentation.marvel.MarvelActivity
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide
+
+class SlideActivity : IntroActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.intro_1)
+
+        isButtonBackVisible = false
+        isButtonNextVisible = false
+
+        addSlide(
+            FragmentSlide.Builder()
+                .background(android.R.color.holo_red_dark)
+                .fragment(R.layout.intro_01)
+                .build()
+
+        )
+
+        addSlide(
+            FragmentSlide.Builder()
+                .background(android.R.color.holo_green_dark)
+                .fragment(R.layout.intro_02)
+                .build()
+        )
+
+        addSlide(
+            FragmentSlide.Builder()
+                .background(android.R.color.holo_orange_dark)
+                .fragment(R.layout.intro_03)
+                .canGoForward(false)
+                .build()
+        )
+
+    }
+
+    public fun openNextActivity(view: View){
+        val intent = Intent(this, MarvelActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
