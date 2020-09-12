d42694e04ff4ea20d44aaf7e2fc8fc3082402208
Carlos Junior <carlosjr.developer@outlook.com>
2020-09-12 15:50:42 -0300

    Initial commit.

---
+++ app/src/main/java/com/carlos/marvel/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.carlos.marvel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by CarlosJr
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
