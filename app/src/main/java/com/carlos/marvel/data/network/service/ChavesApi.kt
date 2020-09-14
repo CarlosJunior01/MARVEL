920aed5612aefde4d53a3dbe0d76379620123d3a
Carlos Junior <carlosjr.developer@outlook.com>
2020-09-14 14:37:19 -0300

    Project Update.

--- app/src/main/java/com/carlos/marvel/data/network/service/ChavesApi.kt
+++ app/src/main/java/com/carlos/marvel/data/network/service/ChavesApi.kt
@@ -2,7 +2,7 @@ package com.carlos.marvel.data.network.service
 
 import java.util.*
 
-val CHAVE_PUBLICA = "c9afd3b8f1c9ec02aec65d71186db389"
-val CHAVE_PRIVADA = "09a671a0a87028618bd9c702475696fd5c905011"
+val CHAVE_PUBLICA = "COLOQUE AQUI SUA CHAVE PUBLIC KEY"
+val CHAVE_PRIVADA = "COLOQUE AQUI SUA CHAVE PRIVATE KEY"
 val TIMESTAMP = (Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis / 1000L).toString()
 val MD5_HASH = "$TIMESTAMP$CHAVE_PRIVADA$CHAVE_PUBLICA"
